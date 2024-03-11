48890b6ed847e0dc771be1722215441885d063b3
mape0908 <159275138+mape0908@users.noreply.github.com>
2024-03-11 22:22:45 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,17 +2,20 @@
  let weight;
 
  // 値の代入
- console.log(68);
+weight = 68;
 
  // 変数の宣言
  let height;
 
  // 値の代入
- console.log(1.7);
+ height = 1.7;
 
 
 // 変数の宣言
 let bmi;
 
  // 値の代入
- console.log((68) /(1.7 * 1.7));
+bmi = weight / (height * height);
+
+ // コンソールへの出力
+ console.log(bmi);
