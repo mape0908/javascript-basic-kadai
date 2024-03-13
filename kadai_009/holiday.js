ae1534c3f6767783957b2248c674f7130cc0fd6b
mape0908 <159275138+mape0908@users.noreply.github.com>
2024-03-13 22:49:39 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,17 +1,11 @@
-const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
-console.log(holidays);
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
-
-
-let index = Math.floor(Math.random() * holidays.length);
-
-
-while (index >= 0) {
+let index = 0;
+while (index !== 15) {
   console.log(holidays[index]);
-  index = Math.floor(Math.random() * holidays.length - 1);
+  index++;
 }
-  
-// 1～16までの数値を出力
-for (let i = 1; i < holidays.length; i++) {
-     console.log(holidays[i - 1]);
-   }
+
+for (let i = 0; i < holidays.length; i++) {
+  console.log(holidays[i]);
+}
